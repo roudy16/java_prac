efb0e93e89f0ce64af5e795ba8e8afd4b44dfa9c
Steve R <roudy@umich.edu>
2017-05-25 10:44:59 -0500

    Prob 50 solved

--- ProjectEuler/src/Problem50.java
+++ ProjectEuler/src/Problem50.java
@@ -1,5 +1,62 @@
+import sequence.RandAccessSeq;
+import sequence.LongPrimeGen;
+
 /**
  * Created by Dell on 5/24/2017.
  */
 public class Problem50 {
+    private static final long limit = 1000000;
+
+    public static void main (String[] args) {
+        LongPrimeGen primegen = new LongPrimeGen();
+        RandAccessSeq primes = new RandAccessSeq(primegen);
+
+        long sum = 0;
+        int left_bound = 0;
+        int right_bound = 0;
+
+        while (sum < limit) {
+            sum += primes.get(right_bound++).longValue();
+        }
+
+        right_bound--;
+        sum -= primes.get(right_bound).longValue();
+
+        int width = right_bound;
+        boolean prime_sum_found = false;
+
+        while (width > 0) {
+            while (sum < limit) {
+                if (LongPrimeGen.isPrime(sum)) {
+                    prime_sum_found = true;
+                    break;
+                }
+                
+                sum -= primes.get(left_bound++).longValue();
+                sum += primes.get(right_bound++).longValue();
+            }
+
+            if (prime_sum_found) {
+                break;
+            }
+
+            width--;
+            for (int i = Math.max(left_bound, width); i < right_bound; i++) {
+                sum -= primes.get(i).longValue();
+            }
+            for (int i = Math.min(left_bound, width) - 1; i >= 0; i--) {
+                sum += primes.get(i).longValue();
+            }
+            
+            left_bound = 0;
+            right_bound = width;
+        }
+        
+        if (!prime_sum_found) {
+            System.out.println("Logic error, should have found a prime sum");
+            return;
+        }
+
+        System.out.println("Width: " + Integer.toString(width) + "   Prime: " + Long.toString(sum));
+    }
 }
--- ProjectEuler/src/sequence/LongPrimeGen.java
+++ ProjectEuler/src/sequence/LongPrimeGen.java
@@ -9,6 +9,54 @@ public class LongPrimeGen implements SeqGen<Long> {
         primes = new ArrayList(Arrays.asList(2L, 3L, 5L, 7L, 11L, 13L, 17L, 19L, 23L, 29L, 31L));
     }
 
+    public static boolean isPrime(long q) {
+        boolean is_prime = false;
+
+        synchronized (primes) {
+            Long temp = primes.get(primes.size() - 1);
+            if (q > temp.longValue()) {
+                // linear search as we grow primes ArrayList
+                do {
+                    temp = seek_next_prime();
+                    if (temp == null) {
+                        break;
+                    }
+
+                    if (q == temp.longValue()) {
+                        is_prime = true;
+                        break;
+                    }
+                } while (q > temp.longValue());
+            }
+            else if (q == temp.longValue()) {
+                is_prime = true;
+            }
+            else {
+                // binary search the existing primes ArrayList
+                int left_bound = 0;
+                int right_bound = primes.size() - 1;
+
+                while (right_bound >= left_bound) {
+                    int mid = left_bound + (right_bound - left_bound) / 2;
+                    long prime = primes.get(mid).longValue();
+
+                    if (q == prime) {
+                        is_prime = true;
+                        break;
+                    }
+
+                    if (q < prime) {
+                        right_bound = mid - 1;
+                    } else {
+                        left_bound = mid + 1;
+                    }
+                }
+            }
+        } // end synchronized (primes)
+
+        return is_prime;
+    }
+
     private int primes_idx;
 
 
