d33059a3bf49c2fa8d2c9213dc0d909d43a63d3a
Steve R <roudy@umich.edu>
2017-05-29 14:13:49 -0500

    Server rough draft

--- PracJavaServer/src/jpserver/RequestHandler.java
+++ PracJavaServer/src/jpserver/RequestHandler.java
@@ -2,6 +2,7 @@ package jpserver;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.UUID;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -12,6 +13,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class RequestHandler implements Runnable {
+    private static final String SUCCESS = "SUCCESS";
+    private static final String ERROR = "ERROR";
     private final Socket client;
     private ServerSocket serverSocket;
 
@@ -28,52 +31,97 @@ public class RequestHandler implements Runnable {
             final String reqContent = in.readLine();
 
             if (reqContent != null && !"".equals(reqContent)) {
-                processReq(reqContent);
+                final UUID reqID = UUID.randomUUID();
+                final RequestResult res = processReq(reqContent, reqID);
+                
+                if (res.getErrCode() != 0) {
+                    System.out.println("Error processing request ID: " + reqID.toString()
+                                      + " Err: " + Integer.toString(res.getErrCode()));
+                }
+                
+                writer.write(res.getResponse());
+            } else {
+                writer.write(ERROR + " content empty");
             }
 
-            writer.write("You entered : " + reqContent);
-            writer.newLine();
             writer.flush();
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             System.out.println("I/O exception: " + e);
-        } catch (Exception ex) {
-            System.out.println("Exceprion in Thread Run. Exception : " + ex);
+        }
+        catch (Exception ex) {
+            System.out.println("Exception in Thread Run: " + ex);
         }
     }
-    
+
     public static void main(String[] args) {
         final RequestHandler rh = new RequestHandler(null);
         final String request = "sort 6 4 11 21 102 3 37";
         final String[] reqWords = request.split(" ");
-        
+
         final Action act = rh.parseAction(reqWords[0]);
         System.out.println(act);
-        
+
         final ArrayList<Integer> sortedNums = rh.sortRequest(reqWords);
         System.out.println(sortedNums);
     }
-    
-    private void processReq(String reqContent) {
-        System.out.println("Received message from " + Thread.currentThread().getName() + " : " + reqContent);
+
+    private RequestResult processReq(String reqContent, final UUID reqID) {
+        System.out.println("Processing request ID: " + reqID.toString() + " on thread: "
+                          + Thread.currentThread().getName() + " : " + reqContent);
 
         final String[] reqWords = reqContent.split(" ");
 
         if (reqWords.length < 2) {
-            System.out.println("WARNING: request did not contain minimum <ACTION> <SIZE>, doing nothing");
-            return;
+            return new RequestResult(ERROR + " request did not contain minimum <ACTION> <SIZE>", -1);
         }
 
         final Action act = parseAction(reqWords[0]);
+        String response = "";
+        int retCode = 0;
 
         switch (act) {
         case SORT:
             final ArrayList<Integer> sortedNums = sortRequest(reqWords);
+            if (sortedNums == null) {
+                retCode = -1;
+                break;
+            }
+
+            final String numStr = toResponseFormat(sortedNums);
+            if (numStr == null || "".equals(numStr)) {
+                retCode = -1;
+                break;
+            }
+
+            response = SUCCESS + ' ' + numStr;
             break;
         case NONE:
         default:
-            System.out.println("WARNING: Action not read properly, doing nothing");
+            retCode = -1;
+            response = ERROR + " Action not read properly";
+
         }
 
+        return new RequestResult(response, retCode);
+    }
+
+    private final class RequestResult {
+        private final String response;
+        private final int errCode;
+
+        private RequestResult(String resp, int err) {
+            response = resp;
+            errCode = err;
+        }
+
+        private String getResponse() {
+            return response;
+        }
+
+        private int getErrCode() {
+            return errCode;
+        }
     }
 
     private ArrayList<Integer> sortRequest(String[] reqWords) {
@@ -93,15 +141,26 @@ public class RequestHandler implements Runnable {
         }
 
         Collections.sort(nums);
-        
+
+        return nums;
+    }
+
+    private String toResponseFormat(ArrayList<Integer> nums) {
+        final int size = nums.size();
+
+        if (size <= 0) {
+            return "";
+        }
+
         final StringBuilder sb = new StringBuilder(size * 2);
         sb.append(nums.get(0).toString());
         for (int i = 1; i < size; i++) {
             sb.append(' ');
             sb.append(nums.get(i).toString());
         }
-        
-        return nums;
+
+        final String resp = sb.toString();
+        return resp;
     }
 
     private enum Action {
